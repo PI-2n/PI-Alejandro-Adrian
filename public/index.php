858922e0ef9f909c5a8d9b7c67927054cbd08b3f
AlejandroRS123 <alericsan@alu.edu.gva.es>
2025-12-02 19:06:09 +0100

    MOD cambiado el header del index para utilizar el generico

--- public/index.php
+++ public/index.php
@@ -48,53 +48,7 @@ $isLogged = !empty($userId);
 </script>
 
 <body>
-  <header>
-    <div class="header_image">
-      <a href="index.html">
-        <img src="img/img_logo.png" alt="Logo" />
-      </a>
-    </div>
-
-    <div class="header_searchBar">
-      <form class="search-form" action="/search" method="get" role="search" aria-label="Buscar en el sitio">
-        <label for="q" class="visually-hidden"></label>
-        <input id="q" class="search-input" name="q" type="search" placeholder="Buscar..." aria-label="Texto de búsqueda"
-          required />
-        <button class="search-btn" type="submit">
-          <img src="img/img_lupa.png" alt="Buscar" />
-        </button>
-      </form>
-    </div>
-
-    <div class="header_btn-container">
-      <div class="platform-btn-container">
-        <a href="" rel="noopener">
-          <img src="img/img_steam.png" alt="Steam" class="platform-btn" />
-        </a>
-        <a href="" rel="noopener">
-          <img src="img/img_ps.png" alt="PlayStation" class="platform-btn" />
-        </a>
-        <a href="" rel="noopener">
-          <img src="img/img_xbox.png" alt="Xbox" class="platform-btn" />
-        </a>
-        <a href="" rel="noopener">
-          <img src="img/img_switch.png" alt="Nintendo Switch" class="platform-btn" />
-        </a>
-        <a href="" rel="noopener">
-          <img src="img/img_pc.png" alt="PC Software" class="platform-btn" />
-        </a>
-      </div>
-      <div class="separator"></div>
-      <div class="user-btn-container">
-        <a href="../frontend/templates/tmp_login.php" rel="noopener">
-          <img src="img/img_user.png" alt="Iniciar Sesión" class="user-btn" />
-        </a>
-        <a href="" rel="noopener">
-          <img src="img/img_carrito.png" alt="Carrito" class="user-btn" />
-        </a>
-      </div>
-    </div>
-  </header>
+    <?php include __DIR__ . '../../frontend/templates/partials/header.php'; ?>
 
   <main>
 
