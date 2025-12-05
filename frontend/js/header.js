91e1acfc21ea7e4e8b9a57e97f49f0e3bd705d76
gug34 <gutionil1@gmail.com>
2025-12-05 15:34:44 +0100

    MOD estilos

--- frontend/js/header.js
+++ frontend/js/header.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('header');
+    const searchInput = document.querySelector('.search-input');
+
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) {
+        if (window.scrollY > 100) {
             header.classList.add('scrolled');
+            searchInput.classList.add('scrolled');
         } else {
             header.classList.remove('scrolled');
+            searchInput.classList.remove('scrolled');
         }
     });
 });
